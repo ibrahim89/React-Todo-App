ecc9657b02231b022f385df6f297e28683999fc8
ibrahim89 <ibrahim89@gmail.com>
2021-08-30 19:56:42 +0530

    new commit

--- src/MyComponent/Footer.js
+++ src/MyComponent/Footer.js
@@ -7,6 +7,7 @@ function Footer() {
         width:"100%"
 
     }
+    
     return (
         <footer className="text-center bg-dark text-light py-3" style={footerStyle} >
             Copyright &copy; Mytodoslist.com
